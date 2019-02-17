1eeda69a9c68023e93ada3ca79db55fa48dcd537
Haoji Liu <haojiliu@haojis-mbp.attlocal.net>
2019-02-17 10:35:32 -0800

    Added grid system in UI, and a sample prediction workflow
    Test Done:
    1. tested on local, can make a predict post request, can load the homepage

--- project/app/routes/index.js
+++ project/app/routes/index.js
@@ -5,14 +5,21 @@ var prediction_controller = require('../controllers/predictionController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'cmpe 280 project' });
+  res.render('index', { title: 'College Admission Lottery!' });
 });
 
 router.get('/about', function (req, res) {
   res.send('About this CMPE 280 project...');
 });
 
+///////////////////////////////
+router.get('/predict', function(req, res, next) {
+  res.render('predict', { title: 'Predict Your Admission!' });
+});
+
 // TODO: Change to a POST request
-router.get('/predict', prediction_controller.prediction_post);
+router.post('/predict', prediction_controller.prediction_post);
+
+
 
 module.exports = router;
