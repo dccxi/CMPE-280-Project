f15c11d33f50728cb765581a92081f869411b6aa
Tong Wu <tongwu@live.com>
2019-02-17 19:31:14 -0800

    Remove code for backend

--- project/app/public/javascripts/business_logic.js
+++ project/app/public/javascripts/business_logic.js
@@ -3,13 +3,6 @@ function redirect(loc) {
   window.location.href = loc;
 }
 
-// TODO: Implement this to make ajax calls to backend
-function getPredictScore(formData) {
-  return {
-    score: 1.234
-  };
-}
-
 // check valid input by ID
 function isValidById() {
   var flag = true;
