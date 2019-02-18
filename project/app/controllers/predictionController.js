a6401f5d6a32ad0171a90a2f0f38e592315f67f2
Tong Wu <tongwu@live.com>
2019-02-17 20:18:50 -0800

    Display prediction result at the end of the page by modifiying DOM

--- project/app/controllers/predictionController.js
+++ project/app/controllers/predictionController.js
@@ -1,22 +1,15 @@
 var Prediction = require('../models/prediction');
 
-var predict = function (gre, toefl) {
+var getPredictScore = function (gre, toefl, res) {
     // return random value as prediction for now (prototyping)
-    return (Math.random() * 100).toString().slice(0, 4);
+    res.send((Math.random() * 100).toString().slice(0, 4));
 }
 
 // Display list of all BookInstances.
 exports.prediction_post = function (req, res) {
     var { name, gre, toefl } = req.body;
     if (name && gre && toefl) {
-        var result = predict(gre, toefl);
-        res.render('predict', {
-            title: 'Prediction Result',
-            name,
-            gre,
-            toefl,
-            result
-        });
+        getPredictScore(gre, toefl, res)
     } else {
         res.send('Invalid operation')
     }
