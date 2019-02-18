4903f6875c1e0f283866629e6b559dfb988fa913
Tong Wu <tongwu@live.com>
2019-02-17 16:22:49 -0800

    Finish controller for prediction (prototype)

--- project/app/controllers/predictionController.js
+++ project/app/controllers/predictionController.js
@@ -1,7 +1,23 @@
 var Prediction = require('../models/prediction');
 
-// Display list of all BookInstances.
-exports.prediction_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Prediction post');
-};
+var predict = function (gre, toefl) {
+    // return random value as prediction for now (prototyping)
+    return (Math.random() * 100).toString().slice(0, 4);
+}
 
+// Display list of all BookInstances.
+exports.prediction_post = function (req, res) {
+    var { name, gre, toefl } = req.body;
+    if (name && gre && toefl) {
+        var result = predict(gre, toefl);
+        res.render('predict', {
+            title: 'Prediction Result',
+            name,
+            gre,
+            toefl,
+            result
+        });
+    } else {
+        res.send('Invalid operation')
+    }
+};
