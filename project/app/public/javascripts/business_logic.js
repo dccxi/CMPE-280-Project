a0535101c0991059f0c89f944765314f320260bc
Haoji Liu <haojiliu@haojis-mbp.attlocal.net>
2019-02-17 16:51:14 -0800

    Moving javascript code to its own file
    including jquery for ajax calls

---
+++ project/app/public/javascripts/business_logic.js
@@ -0,0 +1,11 @@
+// This file contains all js functions for this project
+function redirect(loc) {
+  window.location.href = loc;
+}
+
+// TODO: Implement this, call this from predict.jade
+function getPredictScore(formData) {
+  return {
+    score: 1.234
+  };
+}
